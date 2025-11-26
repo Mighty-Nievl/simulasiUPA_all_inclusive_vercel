13b3a95e03df5c1452d0c941b289b19fe3c42ec9
Mighty-Nievl <rezalhbramantara@gmail.com>
2025-11-26 12:51:10 +0700

    refactor: move login and register to root paths /login and /daftar

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -2,8 +2,8 @@ export const SITE_CONFIG = {
   // Domain config
   rootDomain: process.env.NEXT_PUBLIC_ROOT_DOMAIN || "simupa.web.id",
   appUrl: process.env.NEXT_PUBLIC_APP_URL || "https://app.simupa.web.id",
-  loginUrl: process.env.NEXT_PUBLIC_LOGIN_URL || "https://login.simupa.web.id",
-  registerUrl: process.env.NEXT_PUBLIC_REGISTER_URL || "https://daftar.simupa.web.id",
+  loginUrl: `${process.env.NEXT_PUBLIC_ROOT_DOMAIN ? `http://${process.env.NEXT_PUBLIC_ROOT_DOMAIN}` : "https://simupa.web.id"}/login`,
+  registerUrl: `${process.env.NEXT_PUBLIC_ROOT_DOMAIN ? `http://${process.env.NEXT_PUBLIC_ROOT_DOMAIN}` : "https://simupa.web.id"}/daftar`,
   
   // Other potential config items
   name: "Simulasi UPA PERADI",
